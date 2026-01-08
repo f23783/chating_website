2d732317025dc2cc08d3449e184a6d7c8fada7f2
Emire <gungoremire28@gmail.com>
2026-01-08 17:51:14 +0300

    Fix login page logic
    - Debugged user input handling in login_app.js
    - Connected Sign Up button to register page
    - Added main index.html as the entry point of the frontend

--- Frontend/Login/login_app.js
+++ Frontend/Login/login_app.js
@@ -1,3 +1,5 @@
+//console.log("login_app.js yüklendi");
+
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const signInBtn = document.getElementById("button_signin");
@@ -5,13 +7,21 @@ const signUpBtn = document.getElementById("button_signup");
 const rememberMeBtn = document.getElementById("button_rememberclick");
 const forgotPasswordBtn = document.getElementById("button_forgetclick");
 
+/*
+    signInBtn.addEventListener("click", () => {
+        alert("Clicked to SignIn Button ✅");
+    });
+*/
 
 signInBtn.addEventListener("click", async(event) => {
     event.preventDefault();
 
     const username = usernameInput.value.trim();
     const password = passwordInput.value.trim();
-
+/*
+    console.log("Username:", username);
+    console.log("Password:", password);
+*/
     if (!isInfoValid()) return;
     
     const result = await login(username, password);
@@ -21,6 +31,11 @@ signInBtn.addEventListener("click", async(event) => {
     } else {
         alert(result.message);
     }
+});
+
+
+signUpBtn.addEventListener("click", () => {
+    window.location.href = "../Register/register.html";
 });
 
 
